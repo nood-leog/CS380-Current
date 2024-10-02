b53534b373ebf208a5c36460ec3a3932b25ea7dc
Alex <116304896+nood-leog@users.noreply.github.com>
2024-10-02 11:24:38 -0700

    Update sample.java

--- sample.java
+++ sample.java
@@ -9,7 +9,6 @@ public class sample
         System.out.println("Hello, World!");
         System.out.println("This is some sample code!");
         System.out.println("Its objective is to learn how pull requests work within git!");
-        System.out.println("As this code is much improved from the prior itteration, we know that the branch name is accurate!");
         System.out.println("With any luck, this branch will make sure that I git points for Quiz 2.3!");
         System.out.println("I will merge this change with the main branch!");
     }
